075af0c5f33016ddda657c0c3d110c62cfb588eb
Yu chaeyi <shub29@naver.com>
2023-11-02 05:18:44 +0900

    화면 비율
    빌드했을 때의 화면 비율 조정

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -13,8 +13,8 @@ public class StageManager : MonoBehaviour
 
     private void Awake()
     {
-        Screen.SetResolution(1080, 1920, false);
-    }
+        SetResolution();
+    } 
 
     private void Start()
     {
@@ -29,6 +29,29 @@ public class StageManager : MonoBehaviour
                 break;
             default:
                 break;
+        }
+    }
+
+    //블로그보고 가져온 코드
+    public void SetResolution()
+    {
+        int setWidth = 1080; // 사용자 설정 너비
+        int setHeight = 1920; // 사용자 설정 높이
+
+        int deviceWidth = Screen.width; // 기기 너비 저장
+        int deviceHeight = Screen.height; // 기기 높이 저장
+
+        Screen.SetResolution(setWidth, (int)(((float)deviceHeight / deviceWidth) * setWidth), true); // SetResolution 함수 제대로 사용하기
+
+        if ((float)setWidth / setHeight < (float)deviceWidth / deviceHeight) // 기기의 해상도 비가 더 큰 경우
+        {
+            float newWidth = ((float)setWidth / setHeight) / ((float)deviceWidth / deviceHeight); // 새로운 너비
+            Camera.main.rect = new Rect((1f - newWidth) / 2f, 0f, newWidth, 1f); // 새로운 Rect 적용
+        }
+        else // 게임의 해상도 비가 더 큰 경우
+        {
+            float newHeight = ((float)deviceWidth / deviceHeight) / ((float)setWidth / setHeight); // 새로운 높이
+            Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight); // 새로운 Rect 적용
         }
     }
 
